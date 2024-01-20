6ec15ca2cdfd27a913bb150923a35351ddd7beea
ViniciusBenetti <makako1289@gmail.com>
2024-01-20 13:46:15 -0300

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,5 @@
+import pyautogui
+import time 
+
+time.sleep(5)
+pyautogui.write("Hello world, my name is Vinicius and i'm an 1 year experienced front end developer. My knowledge doesn't cap on it. I also love embbed systems, back end and rapid application development. My main goal is work for myself and alt is work for enterprises. Now, we gonna talk about this project on the screen. This is a readability test project that i made for training my skills and  the differencial is that i'm using the C language to deal with long texts with a high performance since javascript's interpreter is relatively slow. i used python flask API as a controller and ctypes to use a shared '.so' file and put the server on Cloud. This project demanded a high complexity because i used to write code in html,css,python,javascript(fetch) and C(the hardest), but i concluded that i can reuse my C functions with the Power of API. Also C is my main coding language and i trained a lot of skills on it, You will see now the performance to calculate the readability of this text. Good Bye!",interval=0.08)
